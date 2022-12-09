01505fb64e37c4d1954901698cb5cdecdcb4633e
Santiago <sncarp2003@gmail.com>
2022-12-08 20:38:32 -0500

    Segundo

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,52 @@
 public class Main {
     public static void main(String[] args) {
-        // Primera Parte
-        int param1 = 30;
-        int param2 = 56;
-        int param3 = 89;
-        var valor = suma(param1, param2, param3);
-        System.out.println(valor);
+        // 1)
+        var numeroIf = 0;
+        if (numeroIf > 0) {
+            System.out.println("Es positivo");
+        } else if (numeroIf < 0) {
+            System.out.println("Es negativo");
+        } else if (numeroIf == 0) {
+            System.out.println("Es igual a cero");
 
-        // Segunda Parte
-        Coche miCoche = new Coche();
-        miCoche.agregarPuertas();
-        System.out.println(miCoche.puertas);
-    }
+            // 2)
+            int numeroWhile = -5;
+            while (numeroWhile < 3) {
+                System.out.println(numeroWhile);
+                numeroWhile = numeroWhile + 1;
 
-    // Primera Parte
-    public static int suma(int a, int b, int c){
-        return a + b + c;
-    }
-}
-// Segunda Parte
-class Coche {
-    public int puertas = 4;
+                // 3)
+                int numWhile = -2;
+                do {
+                    System.out.println(numWhile);
+                    numWhile = numWhile + 1;
+                }
+                while (numWhile < 3);
 
-    public void agregarPuertas(){
-        this.puertas++;
+                // 4)
+                for (int numeroFor = 0; numeroFor <= 3; numeroFor++) {
+                    System.out.println(numeroFor);
+                }
+
+                // 5)
+                var estacion = "verano";
+                switch (estacion) {
+                    default:
+                        System.out.println("DEFAULT");
+                        break;
+                    case "Primavera":
+                        System.out.println("Es Primavera!!!");
+                        break;
+                    case "Invierno":
+                        System.out.println("Es invierno!!!");
+                        break;
+                    case "verano":
+                        System.out.println("Es verano!!!");
+                        break;
+                }
+
+            }
+        }
     }
 }
 
