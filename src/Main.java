c1c2d65e397bd04f99d2b58cc51547ef0bfe3999
Santiago <sncarp2003@gmail.com>
2023-02-27 17:44:13 -0300

    Tercer

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,46 @@
+
 public class Main {
     public static void main(String[] args) {
-        // 1)
-        var numeroIf = 0;
-        if (numeroIf > 0) {
-            System.out.println("Es positivo");
-        } else if (numeroIf < 0) {
-            System.out.println("Es negativo");
-        } else if (numeroIf == 0) {
-            System.out.println("Es igual a cero");
+        Persona persona = new Persona();
 
-            // 2)
-            int numeroWhile = -5;
-            while (numeroWhile < 3) {
-                System.out.println(numeroWhile);
-                numeroWhile = numeroWhile + 1;
+        persona.setEdad(19);
+        persona.setNombre("Santi");
+        persona.setTelefono("1234567890");
 
-                // 3)
-                int numWhile = -2;
-                do {
-                    System.out.println(numWhile);
-                    numWhile = numWhile + 1;
-                }
-                while (numWhile < 3);
-
-                // 4)
-                for (int numeroFor = 0; numeroFor <= 3; numeroFor++) {
-                    System.out.println(numeroFor);
-                }
-
-                // 5)
-                var estacion = "verano";
-                switch (estacion) {
-                    default:
-                        System.out.println("DEFAULT");
-                        break;
-                    case "Primavera":
-                        System.out.println("Es Primavera!!!");
-                        break;
-                    case "Invierno":
-                        System.out.println("Es invierno!!!");
-                        break;
-                    case "verano":
-                        System.out.println("Es verano!!!");
-                        break;
-                }
-
-            }
-        }
+        System.out.println("Edad: " + persona.getEdad());
+        System.out.println("Nombre: " + persona.getNombre());
+        System.out.println("Teléfono: " + persona.getTelefono());
     }
 }
+
+class Persona {
+    private int edad;
+    private String nombre;
+    private String telefono;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
+
 
